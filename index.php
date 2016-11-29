68471a562bf343cf4cbe59e86b3960d5246583b3
DESKTOP-ED133FV\Ethan <ethanbpatchell@gmail.com>
2016-11-29 21:14:30 +1300

    123

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
 <html>
     <head>
         <title>Playblox.com</title>
+        <link rel="stylesheet" type="text/css" href="CSS/style.css">
     </head>
     <html>
         <div id="about">
-
+           <br />
+            <h2>WHAT IS PLAYBLOX?</h2>
+            <p>Playblox is a place to imagine with friends.</p>
+            <br />
         </div>
     </html>
 </html>
